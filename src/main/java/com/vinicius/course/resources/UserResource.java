f7c3ea61f246a4fdcf10948b034da8234705196c
vinicius-hora <vinicius_hora@live.com>
2021-04-27 18:44:14 -0300

    criado entidades e resource

---
+++ src/main/java/com/vinicius/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.vinicius.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinicius.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@teste.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+		
+		
+	}
+
+}
