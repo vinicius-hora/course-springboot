b89e4c85e8c3b007c29bb9342a8508f6aaca1900
vinicius-hora <vinicius_hora@live.com>
2021-05-02 18:17:28 -0300

    classe enum para pedidos

--- src/main/java/com/vinicius/course/entities/Order.java
+++ src/main/java/com/vinicius/course/entities/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.vinicius.course.entities.entities.enums.OrderStatus;
 
 
 
@@ -29,6 +30,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyy=MM-dd'T'HH:mm:ss'Z'", timezone = "GMT" )
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -37,10 +40,11 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -58,6 +62,17 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
 	}
 
 	public User getClient() {
@@ -92,6 +107,8 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 
