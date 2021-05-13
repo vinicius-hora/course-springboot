78a4418a66b3596f5acf6fca4fa5c173b69cfebf
vinicius-hora <vinicius_hora@live.com>
2021-05-13 19:31:48 -0300

    Associação One-to-many em orderitem

--- src/main/java/com/vinicius/course/entities/OrderItem.java
+++ src/main/java/com/vinicius/course/entities/OrderItem.java
@@ -41,9 +41,11 @@ public class OrderItem  implements Serializable{
 		id.setOrder(order);
 	}
 	
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
+	
 	
 	public void setProduct(Product product) {
 		id.setProduct(product);
