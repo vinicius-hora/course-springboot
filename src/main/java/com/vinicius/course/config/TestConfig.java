eb02efd3fa45c62b4cdd6bf1d7071d47a23bede4
vinicius-hora <vinicius_hora@live.com>
2021-05-13 19:51:16 -0300

    OnetoOne associação

--- src/main/java/com/vinicius/course/config/TestConfig.java
+++ src/main/java/com/vinicius/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vinicius.course.entities.Category;
 import com.vinicius.course.entities.Order;
 import com.vinicius.course.entities.OrderItem;
+import com.vinicius.course.entities.Payment;
 import com.vinicius.course.entities.Product;
 import com.vinicius.course.entities.User;
 import com.vinicius.course.entities.entities.enums.OrderStatus;
@@ -83,7 +84,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 		
