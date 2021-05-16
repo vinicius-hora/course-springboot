e1bb0cca258838ec67bf07d6c9c11d43124c7cb3
vinicius-hora <vinicius_hora@live.com>
2021-05-16 12:13:35 -0300

    Database exception

--- src/main/java/com/vinicius/course/services/UserService.java
+++ src/main/java/com/vinicius/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.vinicius.course.entities.User;
 import com.vinicius.course.repositories.UserRepository;
+import com.vinicius.course.services.exceptions.DatabaseException;
 import com.vinicius.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,15 @@ public class UserService  {
 	}
 	
 	public void delete (long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
